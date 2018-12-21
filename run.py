580b9988850ae3aabef6fe916231387bde509881
gajengi <gajengiramesh@gmail.com>
2018-12-20 22:44:46 -0500

    completed authentication with token

--- run.py
+++ run.py
@@ -2,13 +2,16 @@ from flask import Flask ,Response , jsonify
 from  db.services.context import db_session
 from security.services.authentication  import auth_service
 from security.services.user import user_service
+from flask_cors import CORS
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'TesTing123987546'
+CORS(app)
 
 class MyResponse(Response):
     @classmethod
     def force_type(cls, rv, environ=None):
         print('ramesh')
-        if isinstance(rv, dict):
+        if isinstance(rv, dict) or isinstance(rv,int):
             rv = jsonify(rv)
             return rv
         return super(Response, cls).force_type(rv, environ)
@@ -17,8 +20,9 @@ app.response_class = MyResponse
 app.register_blueprint(auth_service)
 app.register_blueprint(user_service)
 
-class MyResponse(Response):
-    pass
+@app.errorhandler(Exception)
+def handle_bad_request(e):
+    return str(e), 500
 
 @app.route("/")
 def hello():
@@ -31,6 +35,6 @@ def shutdown_session(exception=None):
 
 if __name__ == '__main__':
     from db.services.context import init_db
-
+    from security.models.user import User
     init_db()
     app.run()
--- security/models/user.py
+++ security/models/user.py
@@ -6,7 +6,9 @@ from functools import wraps
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
+    login = Column(String(50), unique=True)
+    password = Column(String(50))
+    name = Column(String(50) )
     email = Column(String(120), unique=True)
 
     def __init__(self, name=None, email=None):
@@ -23,6 +25,6 @@ def returns_user(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         r = f(*args, **kwargs)
-        r = {'name' : r.name,'email' : r.email}
+        r = {'login':r.login,'name' : r.name,'email' : r.email}
         return r
     return decorated_function
